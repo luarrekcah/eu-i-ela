cad243f527dd6200a1759e402f6ca55fcf5c40f9
luarrekcah <raulrodriguesdemoraes@gmail.com>
2025-06-06 00:10:01 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function openModal(id) {
+  document.getElementById(id).style.display = 'block';
+}
+
+function closeModal(id) {
+  document.getElementById(id).style.display = 'none';
+}
+
+// Fecha o modal ao clicar fora do conteúdo
+window.onclick = function(event) {
+  const modals = document.querySelectorAll('.modal');
+  modals.forEach(modal => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+};
